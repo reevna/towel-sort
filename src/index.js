48d206a774a81df871e2d00126386284d2102ed4
github-reevna <shu-@list.ru>
2021-03-15 08:43:24 +0300

    add: solution for empty array

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+if (!(matrix) || matrix == 0) {
+  return []
 }
+
+  for (let i = 1; i < matrix.length; i++ );
+  if (i % 2 == 1) {
+    matrix[j].reverse();
+  }
+  return matrix(i).concat(matrix(j))
+// return matrix.concat();
+
+// return ([].concat(...matrix));
+}
+
+
+
+//it('Should return empty array if no params passed', () => {
+//  assert.deepEqual(towelSort(), []);
+// });
+
+// it('Should return empty array if matrix is empty', () => {
